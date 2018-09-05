d867db981422538a87e449195098b46f4640d4fe
Arnelle Balane <arnellebalane@gmail.com>
2018-09-06 00:32:01 +0800

    Temporarily render StreakWidget in Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,8 @@
     <TheNavbar />
 
     <div :class="$shared.wrapper">
-      <EmptyState />
+      <EmptyState :class="$style.feedWidget" />
+      <StreakWidget :class="$style.feedWidget" />
     </div>
   </div>
 </template>
@@ -11,16 +12,23 @@
 <script>
 import TheNavbar from "@/components/TheNavbar.vue";
 import EmptyState from "@/components/EmptyState.vue";
+import StreakWidget from "@/components/StreakWidget.vue";
 
 export default {
   name: "Home",
 
   components: {
     TheNavbar,
-    EmptyState
+    EmptyState,
+    StreakWidget
   }
 };
 </script>
 
 <!-- eslint-disable-next-line -->
 <style module="$shared" src="@/stylesheets/shared.css"></style>
+<style module>
+.feedWidget {
+  margin: 3.2rem 0;
+}
+</style>
