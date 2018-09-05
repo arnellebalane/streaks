b7919e9ac3f58114819cfeb64d16e907e0ef0a44
Arnelle Balane <arnellebalane@gmail.com>
2018-09-05 13:08:48 +0800

    Define BaseButton component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <button
+    class="base-button"
+    :class="$style.button"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "BaseButton"
+};
+</script>
+
+<style module>
+.button {
+  padding: 0 calc(1.6 / 1.2 * 1em);
+  border: none;
+  border-radius: 3px;
+  font-size: 1.2rem;
+  font-weight: 500;
+  line-height: calc(3.2 / 1.2 * 1em);
+  text-transform: uppercase;
+  color: #fff;
+  background-color: var(--primary-color);
+  cursor: pointer;
+}
+</style>
