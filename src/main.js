b1965dc8f0314382aaf0c074bd68364a3e0cb74a
Arnelle Balane <arnellebalane@gmail.com>
2018-09-05 11:46:38 +0800

    Rename registerServiceWorker.js to register-sw.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "./registerServiceWorker";
+import "./register-sw";
 
 Vue.config.productionTip = false;
 
