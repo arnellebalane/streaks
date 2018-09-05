0645d9052f865a5b1406f0e3a751b5b06383617e
Arnelle Balane <arnellebalane@gmail.com>
2018-09-05 13:01:24 +0800

    Render EmptyState in Home view component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="home">
     <TheNavbar />
+
+    <div :class="$style.wrapper">
+      <EmptyState />
+    </div>
   </div>
 </template>
 
 <script>
 import TheNavbar from "@/components/TheNavbar.vue";
+import EmptyState from "@/components/EmptyState.vue";
 
 export default {
   name: "Home",
 
   components: {
-    TheNavbar
+    TheNavbar,
+    EmptyState
   }
 };
 </script>
+
+<style module>
+.wrapper {
+  width: 63rem;
+  margin: 0 auto;
+}
+</style>
