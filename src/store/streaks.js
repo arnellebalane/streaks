9f4e871d472f88ce518679cbf910d867c998f4fd
Arnelle Balane <arnellebalane@gmail.com>
2018-09-06 10:37:57 +0800

    Calculate month label offsets in store

--- src/store/streaks.js
+++ src/store/streaks.js
@@ -1,9 +1,9 @@
-import { subDays, startOfWeek, endOfWeek } from "date-fns";
+import { subDays, startOfWeek, endOfWeek, isSameMonth, format } from "date-fns";
 
 const WEEKS_IN_YEAR = 52;
 const DAYS_IN_WEEK = 7;
 
-function getWeekRanges(today) {
+function getWeeks(today) {
   let weeks = [];
   for (let i = 0; i < WEEKS_IN_YEAR; i++) {
     const start = startOfWeek(subDays(today, i * DAYS_IN_WEEK));
@@ -13,9 +13,20 @@ function getWeekRanges(today) {
   return weeks;
 }
 
+function getMonths(weeks) {
+  return weeks.reduce((months, week, i) => {
+    if (!isSameMonth(week[0], subDays(week[0], DAYS_IN_WEEK))) {
+      return [...months, [format(week[0], "MMM"), i]];
+    }
+    return months;
+  }, []);
+}
+
 const today = new Date();
-const weeks = getWeekRanges(today);
+const weeks = getWeeks(today);
+const months = getMonths(weeks);
 console.log(weeks);
+console.log(months);
 
 const state = {};
 
