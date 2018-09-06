6951d81e53fa63f6cd169de30846962071d94772
Arnelle Balane <arnellebalane@gmail.com>
2018-09-06 22:06:43 +0800

    Add Vuex action for creating new streaks

--- src/store/streaks.js
+++ src/store/streaks.js
@@ -1,4 +1,5 @@
 import idb from "idb";
+import shortid from "shortid";
 import { getWeeks, getMonths } from "@/lib/graph-utils";
 
 const today = new Date();
@@ -16,10 +17,28 @@ const indexedDB = idb.open("streaks", 1, upgradeDB => {
 
 const state = {
   weeks,
-  months
+  months,
+  streaks: []
+};
+
+const actions = {
+  async createStreak({ commit }, payload) {
+    const data = {
+      id: shortid.generate(),
+      createdAt: new Date(),
+      createdAtTz: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      ...payload
+    };
+
+    const db = await indexedDB;
+    const tx = db.transaction("streaks", "readwrite");
+    tx.objectStore("streaks").put(data);
+    await tx.complete;
+  }
 };
 
 export default {
   namespaced: true,
-  state
+  state,
+  actions
 };
