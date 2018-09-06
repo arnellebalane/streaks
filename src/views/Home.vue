a50bd200d55585fa9738298d0a1a6b25bb9f48de
Arnelle Balane <arnellebalane@gmail.com>
2018-09-06 22:23:43 +0800

    Display StreakWidget for each streak object in Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,12 +5,18 @@
     <div :class="$shared.wrapper">
       <EmptyState :class="$style.feedWidget" />
       <CreateStreakWidget :class="$style.feedWidget" />
-      <StreakWidget :class="$style.feedWidget" />
+      <StreakWidget
+        v-for="streak in streaks"
+        :key="streak.id"
+        :data="streak"
+        :class="$style.feedWidget"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 import TheNavbar from "@/components/TheNavbar.vue";
 import EmptyState from "@/components/EmptyState.vue";
 import StreakWidget from "@/components/StreakWidget.vue";
@@ -24,7 +30,9 @@ export default {
     EmptyState,
     StreakWidget,
     CreateStreakWidget
-  }
+  },
+
+  computed: mapState("streaks", ["streaks"])
 };
 </script>
 
