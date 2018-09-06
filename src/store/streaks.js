e2e0c706756e9cc3f30ed12db02da7c42c7d8e98
Arnelle Balane <arnellebalane@gmail.com>
2018-09-06 22:06:12 +0800

    Create streaks object store in indexedDB

--- src/store/streaks.js
+++ src/store/streaks.js
@@ -1,8 +1,18 @@
+import idb from "idb";
 import { getWeeks, getMonths } from "@/lib/graph-utils";
 
 const today = new Date();
 const weeks = getWeeks(today);
 const months = getMonths(weeks);
+
+const indexedDB = idb.open("streaks", 1, upgradeDB => {
+  switch (upgradeDB.oldVersion) {
+    case 0:
+      upgradeDB.createObjectStore("streaks", {
+        keyPath: "id"
+      });
+  }
+});
 
 const state = {
   weeks,
