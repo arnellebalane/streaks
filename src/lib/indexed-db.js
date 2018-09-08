8e6d5bd733dd6d4b64d9c162b98e1614d0d585d8
Arnelle Balane <arnellebalane@gmail.com>
2018-09-08 12:07:19 +0800

    Add migration to compute streak highest values

--- src/lib/indexed-db.js
+++ src/lib/indexed-db.js
@@ -1,12 +1,38 @@
 import idb from "idb";
 
 const indexedDB = idb.open("streaks", 2, async upgradeDB => {
+  /* eslint-disable no-fallthrough */
   switch (upgradeDB.oldVersion) {
     case 0:
       upgradeDB.createObjectStore("streaks", {
         keyPath: "id"
       });
+    case 1:
+      await computeStreakHighestValues(upgradeDB);
   }
 });
+
+async function computeStreakHighestValues(upgradeDB) {
+  const store = upgradeDB.transaction.objectStore("streaks");
+  const streaks = await store.getAll();
+
+  const updatedStreaks = streaks.map(streak => {
+    if (!streak.values) {
+      return streak;
+    }
+
+    const highestValue = { value: 0, date: null };
+    for (const date in streak.values) {
+      const value = streak.values[date];
+      if (value >= highestValue.value) {
+        highestValue.value = value;
+        highestValue.date = new Date(date);
+      }
+    }
+    return { ...streak, highestValue };
+  });
+
+  await Promise.all(updatedStreaks.map(streak => store.put(streak)));
+}
 
 export default indexedDB;
