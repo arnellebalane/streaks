bf97a91fc70f9e8b87ec641d779c41a8e936cec8
Arnelle Balane <arnellebalane@gmail.com>
2018-09-08 17:18:47 +0800

    Add helper function for calculating current streak

--- src/lib/streak-helpers.js
+++ src/lib/streak-helpers.js
@@ -1,3 +1,8 @@
+import addDays from "date-fns/add_days";
+import subDays from "date-fns/sub_days";
+import differenceInCalendarDays from "date-fns/difference_in_calendar_days";
+import format from "date-fns/format";
+
 export function getHighestValue(streak) {
   const highestValue = { value: 0, date: null };
   for (const date in streak.values) {
@@ -8,4 +13,28 @@ export function getHighestValue(streak) {
     }
   }
   return highestValue.value ? highestValue : null;
+}
+
+export function getCurrentStreak(streak) {
+  const values = streak.values || {};
+  let date = new Date();
+  let dateKey = format(date, "YYYY-MM-DD");
+  if (!values[dateKey]) {
+    date = subDays(date, 1);
+    dateKey = format(date, "YYYY-MM-DD");
+  }
+  if (!values[dateKey]) {
+    return null;
+  }
+
+  const endDate = date;
+  date = subDays(date, 1);
+  dateKey = format(date, "YYYY-MM-DD");
+  while (values[dateKey]) {
+    date = subDays(date, 1);
+    dateKey = format(date, "YYYY-MM-DD");
+  }
+  const startDate = addDays(date, 1);
+  const value = differenceInCalendarDays(endDate, startDate) + 1;
+  return { value, startDate, endDate };
 }
